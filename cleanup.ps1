cc139d08ea5f87c84ec6ec16e96c34a4edc34779
MT0z <mt0z1@pir0z.yxz>
2026-03-02 12:26:13 +0100

    Update 2026-03-02 12:26

--- cleanup.ps1
+++
@@ -1,2 +0,0 @@
-Get-ChildItem -Path $PSScriptRoot -Directory -Recurse -Filter __pycache__ | Remove-Item -Recurse -Force
-Get-ChildItem -Path $PSScriptRoot -Recurse -Filter *.log | Remove-Item -Force
